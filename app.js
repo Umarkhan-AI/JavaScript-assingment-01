d53a50458d6494889308bf1a33c4ca453bfba86e
umarkhan-AI <umerkhan2832006@gmail.com>
2024-09-07 14:27:28 +0500

    add 2 files

---
+++ app.js
@@ -0,0 +1,283 @@
+//  **** chapter no 01 (ALERTS) ****  \\
+      
+    //  Q no 1 \\
+
+    // alert("Hello World");
+
+    // Q no 2 \\
+
+    // alert("Error Please Enter a valid Password");
+
+    // Q no 3 \\
+
+    // alert("Welcome to JS Land.. \n Happy coding" );
+
+    // Q no 4 \\
+
+    // alert("Welcome to JS Land");
+
+    // alert("happy coding \n Prevent this page from aditional dilogs") ;
+
+    // Q no 5 \\
+
+    // console.log("hello! i can write JS in my web browser's console");
+
+    // Q no 6 \\
+
+    // alert("Hello World");
+
+
+    //  **** chapter no 02 (VARIABLES FOR STRINGS) ****  \\
+      
+    //  Q no 1 \\
+
+    // var username ;
+
+    //  Q no 2 \\
+
+    // var fullname = "UmarKhan" ;
+
+    //  Q no 3 \\
+
+    // var message = "Hello World" ;
+
+    // alert(message) ; 
+
+    //  Q no 4 \\
+
+    // var name = "Jhone Doe" ;
+    // alert (name) ;
+    // var age = "15 Year Old" ;
+    // alert (age) ;
+    // var info = "Certifide Mobile App Developer" ;
+    // alert (info) ;
+
+    //  Q no 5 \\
+
+    // var food = "Pizza \n Pizz \n Piz \n Pi" ;
+    // alert (food);
+
+    //  Q no 6 \\
+
+    // var email = "umerkhan@gmail.com" ;
+
+    // alert ("My email address is" + " " + email) ;
+
+    //  Q no 7 \\
+
+    // var book = "'A smarter way to learn JS'"
+
+    // alert ("I am try to learn the book" + " " + book) ;
+
+    //  Q no 8 \\
+
+    // document.write ("Yeh! I can write HTML through JS") ;
+
+    //  Q no 9 \\
+ 
+    // var desing = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”" ;
+
+    // alert (desing);
+
+    // document.write(desing) ;
+
+
+
+     //  **** chapter no 02 (VARIABLES FOR NUMBERS) ****  \\
+      
+    //  Q no 1 \\
+
+    // var age = 15 ;
+
+    // alert("I am"+ " " + age +" " + "Year Old") ;
+
+
+    //  Q no 2 \\
+
+    //  var time = 14 ;
+
+    //  alert ("You have vistied this site" + time + " " + "time") ;
+
+
+    //  Q no 3 \\
+
+    // var birth = 1990 ;
+
+    // document.write ("My Birth is" + " " + birth + "<br>"+ " " +" The DATA type of my declared variable ic numeric") ;
+ 
+    //  Q no 4 \\
+
+    // var vn = "Jhone Doe" ;
+    
+    // var Q = 5 ;
+
+    // var pt = "T Shirts"
+
+    // document.write (vn + " " + "odered" + " " + Q + " " + pt + " " + "on XYZ clothing store") ;
+
+
+      //  **** chapter no 02 (VARIABLE NAMES: LEGAL & ILLEGAL) ****  \\
+      
+    //  Q no 1 \\
+
+    // var name = "umar" , age = 15 , address = "XYZ" ;
+
+    //  Q no 2 \\
+
+    // LEGAL :
+
+    // 1- var name ;
+    // 2- var app_chat ;
+    // 3- var book1 ;
+    // 4- var name$ ;
+    // 5- var $name ;
+
+    // ILLEGAL :
+
+    // 1- var na me ;
+    // 2- var 4appchat ;
+    // 3- var &book ;
+    // 4- var name@ ;
+    // 5- var 854674 ;
+
+
+    //  Q no 3 \\
+
+    // var num = "Number" ;
+    // var dolar = "$" ;
+    // var underScore = "_" ;
+    // var alpha = "Letter" ;
+    // var cs = "Case sensitive" ;
+    // var kw = "KeyWord" ;
+
+    // document.write("<h1>Rules for naming JS variables</h1>" + "<br>" + 
+    //     "Variable names can only contain"+" " + num +"," +" "+ dolar + " " + underScore + " "+  "." +" "+ 
+    // "For example : $my_1variable" + "<br>" + "Variables must begin with a" + " " + alpha + ","+
+    //   dolar + " "+ "or" + underScore + " "+ "." + "For example $name, _name or name" + "<br>"+
+    //  "Variable names are case" +" "+ cs + "<br>"+ "Variable names should not be JS" + " "+
+    // kw ) ;
+
+
+    // *****************************************************\\
+
+        //    Chapter 1 (Alerts)   \\
+
+        // Q no 1
+   
+    //  var fs = "firstname" ;
+    //  var ls =  "lastname";
+    //  var e =  "Email";
+    //  var pn = "phone Number" ;
+    //  var pw = "Password"   ;
+     
+    //  alert(fs) ;
+    //  alert(ls) ;
+    //  alert(e) ;
+    //  alert(pn) ;
+    //  alert(pw) ;
+
+
+        // Q no 2
+
+        // alert ("Learing You are learning JavaScript!")
+
+        // Q no 3
+
+        // alert ("Hello! World")
+
+
+        // Chapter 2 (Variables for string) \\
+
+        // Q no 1
+
+        // var firstName ;
+ 
+        // Q no 2
+
+        // var a ;
+        // a = "apple" ;
+
+        // Q no 3
+
+        // var bestMan = "Charli" ;
+        // var boller  = "Amir" ; 
+
+
+        // Chapter 3 (Variables for numbers) \\
+
+        // Q no 1
+
+        // var caseQty  ;
+
+        // Q no 2
+
+        // var caseQty = 144  ;
+        
+        // Q no 3
+
+        // var num = "9" ;
+        
+        // Q no 4
+
+        // var bat ;
+        // bat = 5 ;
+        
+        // Q no 5
+
+        // the value of orderTotal is : 110 .
+
+        // Q no 6
+
+        // var a = 5 ;
+
+        // a = 5 + 10 ;
+        
+        // alert (a);
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+ 
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
